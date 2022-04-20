0b4505653dd59096ec04fbe682cde808707e5080
Bruno Mendes <brunosmendes89@gmail.com>
2022-04-19 23:14:56 -0300

    Tela de Saída de Veículo

--- src/estacionamento/apresentacao/TelaResumoPagamento.java
+++ src/estacionamento/apresentacao/TelaResumoPagamento.java
@@ -1,5 +1,82 @@
 package estacionamento.apresentacao;
 
-public class TelaResumoPagamento {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import estacionamento.negocio.Movimentacao;
+
+public class TelaResumoPagamento extends JFrame implements ActionListener{
+	
+	private JFrame parent;
+
+	public TelaResumoPagamento(Movimentacao movimentacao,JFrame parent) {
+		this.parent = parent;
+		setResizable(false);
+		setTitle("Tela Resumo de Pagamento");
+		getContentPane().setLayout(null);
+		
+		JLabel lblPlaca = new JLabel("Placa:");
+		lblPlaca.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPlaca.setBounds(109, 40, 46, 14);
+		getContentPane().add(lblPlaca);
+		
+		JLabel lblDataEntrada = new JLabel("Entrada:");
+		lblDataEntrada.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblDataEntrada.setBounds(109, 87, 71, 14);
+		getContentPane().add(lblDataEntrada);
+		
+		JLabel lblDataSaida = new JLabel("Sa\u00EDda:");
+		lblDataSaida.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblDataSaida.setBounds(109, 142, 71, 14);
+		getContentPane().add(lblDataSaida);
+		
+		JLabel lblValor = new JLabel("Valor Total:");
+		lblValor.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblValor.setBounds(109, 192, 71, 14);
+		getContentPane().add(lblValor);
+		
+		JLabel lplValPlaca = new JLabel("[placa]");
+		lplValPlaca.setForeground(Color.BLUE);
+		lplValPlaca.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lplValPlaca.setBounds(240, 40, 108, 14);
+		getContentPane().add(lplValPlaca);
+		
+		JLabel lplDataEntrada = new JLabel("[data entrada]");
+		lplDataEntrada.setForeground(Color.BLUE);
+		lplDataEntrada.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lplDataEntrada.setBounds(240, 87, 108, 14);
+		getContentPane().add(lplDataEntrada);
+		
+		JLabel lplDataSaida = new JLabel("[data saida]");
+		lplDataSaida.setForeground(Color.BLUE);
+		lplDataSaida.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lplDataSaida.setBounds(240, 142, 108, 14);
+		getContentPane().add(lplDataSaida);
+		
+		JLabel lplValor = new JLabel("[valor]");
+		lplValor.setForeground(Color.RED);
+		lplValor.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lplValor.setBounds(240, 192, 108, 14);
+		getContentPane().add(lplValor);
+		
+		JButton btnOk = new JButton("Ok");
+		btnOk.addActionListener(this);
+		btnOk.setBounds(163, 227, 89, 23);
+		getContentPane().add(btnOk);
+		
+		setLocationRelativeTo(null);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evento) {
+		parent.setVisible(true);
+		dispose();
+		
+	}
 }
--- src/estacionamento/apresentacao/TelaSaidaVeiculo.java
+++ src/estacionamento/apresentacao/TelaSaidaVeiculo.java
@@ -1,13 +1,85 @@
 package estacionamento.apresentacao;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
 
-public class TelaSaidaVeiculo extends JFrame {
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.text.MaskFormatter;
+
+import estacionamento.controller.EstacionamentoController;
+import estacionamento.negocio.Movimentacao;
+
+public class TelaSaidaVeiculo extends JFrame implements ActionListener {
 
 	private JFrame parent;
+	private JFormattedTextField txtPlaca;
+	
 	public TelaSaidaVeiculo(JFrame parent) {
-		// TODO Auto-generated constructor stub
+		setResizable(false);
+		setTitle("Sa\u00EDda de Ve\u00EDculo");
+		setSize(new Dimension(540, 180));
 		this.parent = parent;
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.SOUTH);
+		
+		JButton btnOk = new JButton("Ok");
+		btnOk.addActionListener(this);
+		btnOk.setActionCommand("ok");
+		panel.add(btnOk);
+		
+		JButton btnCancel = new JButton("Cancelar");
+		btnCancel.addActionListener(this);
+		btnCancel.setActionCommand("cancel");
+		panel.add(btnCancel);
+		
+		JPanel panel_1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		flowLayout.setVgap(35);
+		getContentPane().add(panel_1, BorderLayout.CENTER);
+		
+		JLabel lblPlaca = new JLabel("Placa:");
+		lblPlaca.setFont(new Font("Tahoma", Font.BOLD, 16));
+		panel_1.add(lblPlaca);
+		
+		try {
+			txtPlaca = new JFormattedTextField(new MaskFormatter("UUU-####"));
+		} catch (ParseException e) {
+			assert false : "Formato do padrão inválido!";
+		}
+				
+		txtPlaca.setForeground(Color.BLUE);
+		txtPlaca.setFont(new Font("Tahoma", Font.BOLD, 16));
+		txtPlaca.setColumns(10);
+		panel_1.add(txtPlaca);
+		
+		setLocationRelativeTo(null);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evento) {
+		String cmd = evento.getActionCommand();
+		if(cmd.equals("ok")){
+			//vai para a tela de pagamento
+			EstacionamentoController controle = new EstacionamentoController();
+			Movimentacao movimentacao = controle.processarSarida(txtPlaca.getText());
+			TelaResumoPagamento telaResumo = new TelaResumoPagamento(movimentacao, parent);
+			telaResumo.setVisible(true);
+		}else {
+			parent.setVisible(true);
+		}
+		
+		dispose();
 	}
 
 	
